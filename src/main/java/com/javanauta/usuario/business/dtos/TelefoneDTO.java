039301f4a22a01f37892967adbd385ddb75220a6
Enthone <enthoneferreira9@gmail.com>
2026-04-18 16:17:10 -0300

    adiciona métodos para salvar usuários

---
+++ src/main/java/com/javanauta/usuario/business/dtos/TelefoneDTO.java
@@ -0,0 +1,14 @@
+package com.javanauta.usuario.business.dtos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TelefoneDTO {
+
+    private String numero;
+    private String ddd;
+}
