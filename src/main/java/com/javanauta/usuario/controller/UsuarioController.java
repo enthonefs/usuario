28c5927c1dd5c7a41dae6dc4253b54acb3f847f2
Enthone <enthoneferreira9@gmail.com>
2026-04-22 14:37:02 -0300

    inclusão de método put de usuario

--- src/main/java/com/javanauta/usuario/controller/UsuarioController.java
+++ src/main/java/com/javanauta/usuario/controller/UsuarioController.java
@@ -42,4 +42,10 @@ public class UsuarioController {
         usuarioService.deletarPorEmail(email);
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping
+    public ResponseEntity<UsuarioDTO> atualizaDadosUsuario(@RequestBody UsuarioDTO dto,
+                                                           @RequestHeader("Authorization") String token){
+        return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token, dto));
+    }
 }
